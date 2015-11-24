ee324c82b469aa0e7439e4d6943f29ecbf32fb57
Dave Charnuska <davecharnuska@gmail.com>
2015-11-24 16:40:45 -0500

    continue calc logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
 	var total = 0;
+	var currentOp = null;
+	var currentNum = ""
 	
 	var $button = $(".calculator li");
 	$button.click(function() {
 		var thisClass = $(this).attr("class");
+		var thisValue = $(this).attr("id");
 		if (thisClass === "num") {
-			console.log("num");
+			if (currenOp) {
+				currentNum = currentNum + thisValue;
+			} else {
+				
+			}
+			
 		} else {
-			console.log("op");
+			currentOp = thisValue;
 		}
+		console.log(total);
 	})
 });
