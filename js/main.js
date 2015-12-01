612757170da7d700fb2c2edae5cca749c5309933
Dave Charnuska <davecharnuska@gmail.com>
2015-11-30 23:56:08 -0500

    clean up js, fix typos

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 	var total = 0;
 	var currentOp = null;
-	var currentNum = ""
+	var num1 = null;
+	var num2 = null;
 	
 	var $button = $(".calculator li");
 	$button.click(function() {
 		var thisClass = $(this).attr("class");
 		var thisValue = $(this).attr("id");
 		if (thisClass === "num") {
-			if (currenOp) {
-				currentNum = currentNum + thisValue;
+			if (!num1) {
+				num1 = thisValue;
 			} else {
-				
+				num2 = thisValue;
 			}
-			
 		} else {
 			currentOp = thisValue;
 		}
